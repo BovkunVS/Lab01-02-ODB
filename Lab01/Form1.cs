c8dad09c3ef51e4f52d3b0d4135bc4bb85995bfc
Reckless Lime <valentinkabovkuns@gmail.com>
2024-02-13 13:30:46 +0200

    Add block try-catch-finally and GetConnectionString()

--- Lab01/Form1.cs
+++ Lab01/Form1.cs
@@ -1,3 +1,4 @@
+using Microsoft.Data.SqlClient;
 using System.Configuration;
 using System.Data.Common;
 
@@ -7,8 +8,26 @@ namespace Lab01
     {
         public Form1()
         {
+            // Вызов диалогового окна с инфой о подключении к БД
+            string connectionString = GetConnectionString();
+            MessageBox.Show($"Строка подключения:\n{connectionString}", "Подключение к базе данных", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
             InitializeComponent();
+
             DbProviderFactories.RegisterFactory("Microsoft.Data.SqlClient", Microsoft.Data.SqlClient.SqlClientFactory.Instance);
+        }
+
+        private static string GetConnectionString()
+        {
+            var connect = new SqlConnectionStringBuilder();
+            connect.InitialCatalog = "Exibition";
+            connect.DataSource = @"VDBS";
+            connect.PersistSecurityInfo = true;
+            connect.UserID = "dbuser";
+            connect.Password = "dbuser";
+            connect.Encrypt = false;
+            connect.ConnectTimeout = 30;
+            return connect.ConnectionString;
         }
 
         private void loadingToolStripMenuItem_Click(object sender, EventArgs e)
@@ -16,21 +35,26 @@ namespace Lab01
             rtbResult.Clear();
             // Получение строки подключения и поставщика из *.config
             string dp = ConfigurationManager.AppSettings["provider"];
-            string cnStr = ConfigurationManager.AppSettings["connectionString"];
+            //string cnStr = ConfigurationManager.AppSettings["connectionString"];
+            string cnStr = GetConnectionString();
 
             // Получение генератора поставщика
             DbProviderFactory df = DbProviderFactories.GetFactory(dp);
 
-            using (DbConnection cn = df.CreateConnection())
+            DbConnection cn = df.CreateConnection();
+
+            try
             {
                 // Вывод объекта подключения
                 rtbResult.AppendText("Объект подключения --> " + cn.GetType().Name + "\n");
 
                 cn.ConnectionString = cnStr;
+                //Открыть подключение
                 cn.Open();
+
                 // Создание объекта команды
                 DbCommand cmd = df.CreateCommand();
-                rtbResult.AppendText("Объект команды --> " + df.CreateCommand().GetType().Name + "\n");
+                rtbResult.AppendText("Объект команды --> " + cmd.GetType().Name + "\n");
 
                 cmd.Connection = cn;
                 cmd.CommandText = "Select * From Company";
@@ -47,6 +71,17 @@ namespace Lab01
                     }
                 }
             }
+            catch (DbException ex)
+            {
+                // Протоколировать исключение
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // Гарантировать высвобождение подключения
+                cn.Close();
+            }
+
         }
 
     }
